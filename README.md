ec547e1741dfd4c636a2c01ad0060e0fc843e374
eric mills <emil0927@uni.sydney.edu.au>
2024-06-20 19:24:19 +1000

    Added readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Options Market Simulator
+
+This project uses a variety of statistical inferencing techniques I have learned as well as the binomial options pricing model to simulate an options market.
+
+The simulator utilises the Tkinter python module as a GUI for the user to make decisions.
+
+gui_class.py abstracts the buttons and GUI functionality for the project
+
+random_variable.py randomly calculates the security fluctuations according to a given random variable.
+
+options_pricing.py calculates the fair price of the options according to the Cox Ross Rubinstein options pricing model.
+
+This program will be updated as I complete MATH3975 Financial Derivatives (Advanced)
--- gui_class.py
+++ gui_class.py
@@ -27,7 +27,7 @@ class Root_Wrapper():
         # self.root.destroy() 
         sys.exit() 
 
-class Screen():
+class Screen(): 
     def __init__(self, rootw):
         self.rootw = rootw
         self.frame = tk.Frame(self.rootw.root)
@@ -48,20 +48,3 @@ class Screen():
         canvas.draw()
         canvas.get_tk_widget().place(x=options["x"], y=options["y"])
         self.graphs[options["name"]] = canvas
-
-rootw = Root_Wrapper()
-rootw.add_screen("screen1")
-rootw.add_screen("screen2")
-
-rootw.screens["screen1"].add_button(name="button1", text="Click me!", bg="blue", fg="white", font=("Arial", 12), width=2, height=2, x=175, y=370)
-rootw.screens["screen1"].add_graph(name="graph1", datax=[1, 2, 3, 4, 5], datay=[1, 2, 3, 4, 5], width=2, height=2, x=50, y=50)
-rootw.screens["screen1"].buttons["button1"].config(command=lambda: rootw.switch_screen("screen2"))
-
-rootw.screens["screen2"].add_button(name="button2", text="yaya", bg="blue", fg="white", font=("Arial", 12), width=2, height=2, x=175, y=370)
-
-rootw.switch_screen("screen1")
-rootw.root.mainloop()
-
-
-
-
